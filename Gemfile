3c17a9dd7be2e084962486b1db6fc4a07de3a2c2
Jun Ukemori <jukemori@gmail.com>
2023-05-10 12:12:35 +0900

    models set

--- Gemfile
+++ Gemfile
@@ -27,6 +27,12 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+#Simple form
+gem "simple_form"
+
+#faker
+gem "faker"
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  restaurant.save
+end
+
+10.times do
+  review = Review.new(
+    rating: rand(0..5),
+    content: Faker::Restaurant.review
+  )
+  review.save
+end
